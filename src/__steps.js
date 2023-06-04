0c1b148ce712fa678f267cc8964acefe904597bc
capnemo20 <z.sujon20@outlook.com>
2023-06-05 00:40:39 +0100

    54-1 Another Recap of React Router Setup

---
+++ src/__steps.js
@@ -0,0 +1,15 @@
+/*
+
+Basic router setup
+
+1. npm i react-router-dom
+2. create a route using createRouterBrowser 
+3. add RouterProver and set router props
+4. create lots of components
+5. create Main layout with some common parts and some changing parts based on the route
+6. set main layout at the root of the route
+7. set children routes
+8. set links on the header components
+
+
+*/
